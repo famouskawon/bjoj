f668ddf499e2ebd967da0fb5e289d600575e18ee
famouskawon <leekawon1995@naver.com>
2023-03-21 19:10:08 +0900

    add 2751

---
+++ 006_Silver_V/2751.py
@@ -0,0 +1,12 @@
+import sys 
+
+N = int(sys.stdin.readline().rstrip())
+L = []
+
+for _ in range(N):
+    s = int(sys.stdin.readline().rstrip())
+    L.append(s)
+
+L.sort()
+for i in S:
+    print(i)
